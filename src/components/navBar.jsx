e7a5fcc882116e9e74b029de811f59eb10374100
ishimwegloire01 <ishimwegloire01@gmail.com>
2025-05-19 00:01:35 +0200

    add components

---
+++ src/components/navBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../App.css'
+
+
+function navBar() {
+  return (
+    <>
+      <nav>
+        <h2>Gloire</h2>
+      </nav>
+    </>
+  )
+}
+
+export default navBar
